107a86f30ed11ce4e484cf976a9adf02c0f41e51
alvikram <alvikram@github.com>
2022-12-21 17:46:55 +0000

    Congrats Card-react

---
+++ index.js
@@ -0,0 +1,21 @@
+const element = (
+  <div className="congrats-card">
+    <h1 className="heading">Congratulations</h1>
+    <div className="card-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="profile-image"
+      />
+      <p className="name-card">Kiran V</p>
+      <p className="college-name">
+        Vishnu Institute of Computer Education and Technology, Bhimavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="watch-image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
